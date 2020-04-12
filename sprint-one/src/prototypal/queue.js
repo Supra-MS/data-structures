04b5ccec47c0cf502b68ce58c685e0fe3225ce2b
Supra-MS <suprabharathi.m.s@gmail.com>
2020-04-12 00:57:01 -0700

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.front = 0;
+  someInstance.rear = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.rear++] = value;
+  },
+  dequeue: function() {
+    if (this.rear > this.front) {
+      var deletedValue = this.storage[this.front];
+      delete this.storage[this.front++];
+      return deletedValue;
+    }
+  },
+  size: function() {
+    return this.rear - this.front;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // To create a prototypal chain
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.size()] = value;
+  },
+  pop: function() {
+    var poppedItem = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return poppedItem;
+  },
+  size: function() {
+    var size = Object.keys(this.storage).length;
+    return size;
+  }
+};
 
 
